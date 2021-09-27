be8f6b965a14c545868ae9502e476445e9d5de04
WatchWatch1 <91450685+WatchWatch1@users.noreply.github.com>
2021-09-27 15:51:01 +0900

    Create main2.py

---
+++ 1강/main2.py
@@ -0,0 +1,20 @@
+a = 123
+print(a)
+a = 10291092019204720470213
+print(a)
+a = -1920198092819829
+print(a)
+a = -1.2928921387283728
+print(a)
+a = 0o123
+print(a)
+a = 0x9823fadf
+print(a)
+a = 12
+b = 9123
+print(a+b)
+print(a-b)
+print(b*a)
+print(a/b)
+print(a**b)
+print(a%b)
