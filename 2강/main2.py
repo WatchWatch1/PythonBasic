f666ad3d2ec6c4ce84e16da6aa5d1223408cf573
WatchWatch1 <91450685+WatchWatch1@users.noreply.github.com>
2021-09-29 15:54:11 +0900

    Create main2.py

---
+++ 2강/main2.py
@@ -0,0 +1,30 @@
+string = """오늘 날씨는 흐림입니다 ㅋㅋ \n 나는 오늘 기분이 별로입니다."""
+print(string)
+head = "파이썬은"
+tail = " 재미있다."
+print(head + tail)
+string = "오늘 날씨는 별로입니다."
+heat = "\n 나는 기분이 좋습니다."
+print(string + heat)
+print("head" + "tail")
+a = "파이썬 python"
+print(a*2)
+# 문자열 곱하기 응용 30개
+asdfasdf = "INFJ INFJ INFJ "
+#print(asdfasdf*12312313)
+a = "여의도역은 여기에서 가깝다."
+print(len(a))
+asdfasdfasdf = '''오늘 날씨는 좋지 않습니다. \n 나는 오늘 기분이 좋습니다.'''
+print(len(asdfasdfasdf))
+a = "Life is too short, You need Python"
+print (a[0])
+print (a[1])
+print (a[0]+a[1]+a[2]+a[3]+a[4])
+print (a[0:4])
+print (a[5:7])
+print (a[8:12])
+print (a[12:18])
+print (a[19:22])
+print (a[23:27])
+print (a[28:36])
+print (a[0:4] + a[5:7] + a[8:12] + a[12:18] + a[19:22] + a[23:27] + a[28:36])
