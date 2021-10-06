3d9bd59767d019dea1b51a6222701ae19e177216
WatchWatch1 <91450685+WatchWatch1@users.noreply.github.com>
2021-10-06 15:49:18 +0900

    Create main1.py

---
+++ 3강/main1.py
@@ -0,0 +1,25 @@
+print("Hello, World!")
+print('Hello, World!')
+print("""Hello, World!""")
+print('''Hello, World!''')
+age = 13
+print("나이:" ,age)
+age = 13
+print (age)
+필통 = 1
+공책 = 3
+교과서 = 1
+print(공책, 필통, 교과서)
+print("공책:", 공책 , "필통:", 필통, "교과서:", 교과서)
+string = '''오늘 날씨는 _____
+나는 오늘  _____'''
+print(string)
+hello = "안녕하세요!"
+print(hello)
+num = 5
+hi = ("안녕!")
+hello = ("안녕하세요! ㅋㅋ")
+num = 15
+string = "15"
+print (num)
+print (string)
