1d193decaa7a1ed55bdc42b3bc3c34a76d2d2d70
WatchWatch1 <91450685+WatchWatch1@users.noreply.github.com>
2021-10-13 15:45:39 +0900

    Create main1.py

---
+++ 4강/main1.py
@@ -0,0 +1,117 @@
+hello = "안녕하세요! 오늘은 기분이 좋습니다."
+print(hello[5])
+hello = "\"안녕.\" 철수가 인사를 했다."
+print(hello[10])
+hello = '\'안녕하세요!\' 내가 인사를 건넸다.'
+print(hello[5])
+letters = "python"
+print(letters[0])
+print(letters[2])
+번호판 = "24가 1234"
+print(번호판[4:8])
+파이썬글자= "Life is too short, You need Python"
+print(파이썬글자[0])
+print(파이썬글자[8])
+print(파이썬글자[2])
+print(파이썬글자[9])
+print(파이썬글자[19])
+print(파이썬글자[28])
+hello = "안녕하세요!"
+print(hello[5])
+print(hello[-1])
+파이썬파이썬= "Life is too short, You need Python"
+print(파이썬파이썬[-0])
+슬라이싱= "Life is too short, You need Python"
+print(슬라이싱[19:])
+print(슬라이싱[:17])
+print(슬라이싱[:])
+print(슬라이싱[19:-7])
+print(슬라이싱[0:4])
+print(슬라이싱[5:8])
+print(슬라이싱[8:12])
+print(슬라이싱[12:18])
+print(슬라이싱[19:23])
+print(슬라이싱[23:28])
+print(슬라이싱[28:])
+날씨 = "20211006Rainy"
+날짜 = 날씨[:8]
+날씨이 = 날씨[8:]
+print(날짜)
+print(날씨이)
+a = "20211006Rainy"
+year = a[:4]
+day = a[4:8]
+weather = a[8:]
+print(year)
+print(day)
+print(weather)
+차번호판 = "24가 1234"
+print(차번호판[-4:])
+a = "우리 강아지 이름은 초코입니다"
+print(a[11:13])
+a = "3"
+b = "4"
+print(a+b)
+a = 3
+b = 4
+print(a+b)
+print("Hi" *3)
+print("Hi!" *3)
+print("-" * 80)
+s1 = "python"
+s2 = "java"
+s3 = s1 + ' ' + s2 + ' '
+print(s3 * 4)
+s1 = "좋은 아침입니다!"
+s2 = s1[0:5] + s1[-1]
+print(s2)
+print("나는 사과 %d개를 먹었다." % 3)
+print("나는 사과 {0}개를 먹었다.".format(3))
+print("나는 사과 %s개를 먹었다." % "다섯")
+print("나는 사과 {0}개를 먹었다.".format("다섯"))
+number = 3
+destination = "학교"
+print("나는 사과 %d개를 먹었다. 그리고 %s에 갔다." % (number, destination))
+name1 = "김철수"
+age1 = 10
+name2 = "홍길동"
+age2 = 13
+print("이름: %s 나이: %d" % (name1, age1))
+print("이름: %s 나이: %d" % (name2, age2))
+a = 3
+b = 4
+def add(a, b): return a+b
+a = 3
+b = 4
+c = add(a,b)
+print(c)
+a = 3
+b = 4
+c = add(a,b)
+print(c)
+print(add(10, 15))
+print(add(2345889, 194943))
+print(add(3, 100000000000))
+def minus(a,b): return a-b
+a = 124
+b = 123
+c = minus(a,b)
+print(c)
+def multi(a,b): return a*b
+def divi(a,b): return a//b
+a = 6
+b = 3
+print(add(a,b))
+print(minus(a,b))
+print(multi(a,b))
+print(divi(a,b))
+a= 6
+b= 3
+print(add(b,a))
+print(minus(b,a))
+print(multi(b,a))
+print(divi(b,a))
+def Hello(a,b): return "안녕하세요"
+a= 123
+b= 123
+print(Hello(a,b))
