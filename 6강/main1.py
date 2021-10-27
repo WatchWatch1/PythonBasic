d4594f696e44cf582245eef12809b1de9d647dcf
WatchWatch1 <91450685+WatchWatch1@users.noreply.github.com>
2021-10-27 15:45:33 +0900

    Create main1.py

---
+++ 6강/main1.py
@@ -0,0 +1,62 @@
+money = 10000
+if money == 10000:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
+if 3<5:
+    print("조건식이 True")
+if 3>5:
+    print("조건식이 False")
+apple = 10
+if apple >= 5:
+    print("사과를 나누어 먹는다")
+else:
+    print("사과를 혼자 먹는다")
+apple = 3
+if apple >= 5:
+    print("사과를 나누어 먹는다")
+else:
+    print("사과를 혼자 먹는다")
+money = 10000
+if money >= 3000:
+    print("택시를 타고 가라")
+else:
+    print("걸어 가라")
+number = 15
+if number % 3 == 0:
+    print("{}는 3의 배수입니다.".format(number))
+number = 16
+if number % 3 == 0:
+    print("{}는 3의 배수입니다.".format (number))
+grade = int(input("몇 학년인가요(1~6)?"))
+if grade >= 2 and grade <= 4:
+    print("햄버거를 주세요~!")
+else:
+    print("김밥을 주세요~!")
+rank = int(input("몇 등인가요(1 또는 2)?"))
+if rank == '1':
+    print("쉬어라")
+else:
+    print("설거지를 해라")
+pizza = input('피자 가게가 열렸나요?(예/아니요)')
+chicken = input('치킨 가게가 열렸나요?(예/아니요)')
+bibimbap = input("한식집이 열렸나요?(예/아니요)")
+if pizza == '예':
+    print("피자 가게 ㄱㄱ")
+elif chicken == "예":
+    print("치킨집 ㄱㄱ")
+elif bibimbap == "예":
+    print("한식집 ㄱㄱ")
+else:
+    print("편의점에서 라면 ㄱ")
+print(3 > 5)
+print(3 < 5)
+print(3 == 5)
+print(3 != 5)
+x = 4
+print(1 < x < 5)
+print ((3 == 3) and (4 !=3))
+if 3>5:
+    print ("3은 5보다 크다.")
+if 3>5:
+    print("3은 5보다 작다.")
